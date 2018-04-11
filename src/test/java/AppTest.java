c660c4cd42b6fdc18120f70fbfa3a152ede208f7
Aditya <sp.aditya@gmail.com>
2018-04-11 19:44:41 +0530

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,6 +7,16 @@ public class AppTest
 	@Test
 	public void testApp()
     {
-        assertEquals(1,new App().calculateSomething());
+        assertEquals(0,new App().calculateSomething());
+    }
+	@Test
+	public void testApp1()
+    {
+        assertEquals(0,new App().calculateSomething());
+    }
+	@Test
+	public void testApp2()
+    {
+        assertEquals(0,new App().calculateSomething());
     }
 }
