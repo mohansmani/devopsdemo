7f3ea19d260ed6c6280be503905faccf792bea12
Aditya <sp.aditya@gmail.com>
2018-04-11 17:57:03 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World there !");
+        System.out.println( "Hello World there !")
     }
 
 	public int calculateSomething() {
